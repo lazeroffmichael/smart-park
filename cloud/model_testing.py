4a170791880329c29be39a28b9aa14f258e277db
Michael Lazeroff <michaeldlazeroff@gmail.com>
2021-07-23 20:09:08 -0700

    Push model to cloud

--- cloud/model_testing.py
+++ cloud/model_testing.py
@@ -1,4 +1,32 @@
-import tensorflow
+from google.api_core.client_options import ClientOptions
+from googleapiclient import discovery
+import os
+from sklearn.preprocessing import MinMaxScaler
+import joblib
+import tensorflow as tf
+import pandas as pd
 
-new_model = tensorflow.keras.models.load_model('../model/saved-model')
+ENDPOINT = f"https://{os.getenv('REGION')}-ml.googleapis.com"
+CLIENT_OPTIONS = ClientOptions(api_endpoint=ENDPOINT)
+ML = discovery.build('ml', 'v1', client_options=CLIENT_OPTIONS)
+SCALER = joblib.load('../model/scaler.gz')
 
+scaler = MinMaxScaler()
+test_data_enter = [[36.11128658900951, -115.1407443542069, 36.11138568488752, -115.1407532525297, 36.11145950467051,
+             -115.1407476558897, 36.11157110102095, -115.1407108213047]]
+transformed_data = SCALER.transform(test_data_enter)
+
+request_body = {
+    'instances': transformed_data.tolist()
+}
+name=f"projects/{os.getenv('projectId')}/models/{os.getenv('MODEL_NAME')}/versions/{os.getenv('VERSION_NAME')}"
+request = ML.projects().predict(
+    name=name,
+    body=request_body
+)
+response = request.execute()
+for item in response['predictions']:
+    if item[0] > 0.5:
+        print(1)
+    else:
+        print(0)
--- tests/integration/test_cloud_model.py
+++ tests/integration/test_cloud_model.py
@@ -2,23 +2,32 @@ from google.api_core.client_options import ClientOptions
 from googleapiclient import discovery
 import os
 from sklearn.preprocessing import MinMaxScaler
+import joblib
+import tensorflow as tf
+import pandas as pd
 
 ENDPOINT = f"https://{os.getenv('REGION')}-ml.googleapis.com"
 CLIENT_OPTIONS = ClientOptions(api_endpoint=ENDPOINT)
 ML = discovery.build('ml', 'v1', client_options=CLIENT_OPTIONS)
+SCALER = joblib.load('../../model/scaler.gz')
+MODEL = tf.keras.models.load_model('../../model/saved-model')
 
 
 def test_example():
     scaler = MinMaxScaler()
-    test_data = [36.11128658900951, -115.1407443542069, 36.11138568488752, -115.1407532525297, 36.11145950467051,
-                 -115.1407476558897, 36.11157110102095, -115.1407108213047]
-    transformed_data = scaler.fit_transform(test_data)
+    test_data = [[36.11128658900951, -115.1407443542069, 36.11138568488752, -115.1407532525297, 36.11145950467051,
+                 -115.1407476558897, 36.11157110102095, -115.1407108213047]]
+    transformed_data = SCALER.transform(test_data)
+
     request_body = {
-        'instances': transformed_data
+        'instances': transformed_data.tolist()
     }
+    name=f"projects/{os.getenv('projectId')}/models/{os.getenv('MODEL_NAME')}/versions/{os.getenv('VERSION_NAME')}"
     request = ML.projects().predict(
-        name=f"projects/{os.getenv('projectId')}/models/{os.getenv('MODEL_NAME')}/{os.getenv('VERSION_NAME')}",
+        name=name,
         body=request_body
     )
     response = request.execute()
-    print(response)
+    predictions = pd.DataFrame(response['predictions'])
+    print((predictions > 0.5).astype("int32"))
+
